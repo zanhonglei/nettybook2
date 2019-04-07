3b5004ca97dbb6b55cf7dd59194fee4ba090fc1c
zan <747060040@qq.com>
2019-04-07 20:30:28 +0800

    整理BIO

--- src/com/phei/netty/bio/TimeClient.java
+++ src/com/phei/netty/bio/TimeClient.java
@@ -15,16 +15,13 @@
  */
 package com.phei.netty.bio;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 
 /**
  * @author lilinfeng
- * @date 2014年2月14日
  * @version 1.0
+ * @date 2014年2月14日
  */
 public class TimeClient {
 
@@ -32,54 +29,24 @@ public class TimeClient {
      * @param args
      */
     public static void main(String[] args) {
-
-	int port = 8080;
-	if (args != null && args.length > 0) {
-
-	    try {
-		port = Integer.valueOf(args[0]);
-	    } catch (NumberFormatException e) {
-		// 采用默认值
-	    }
-
-	}
-	Socket socket = null;
-	BufferedReader in = null;
-	PrintWriter out = null;
-	try {
-	    socket = new Socket("127.0.0.1", port);
-	    in = new BufferedReader(new InputStreamReader(
-		    socket.getInputStream()));
-	    out = new PrintWriter(socket.getOutputStream(), true);
-	    out.println("QUERY TIME ORDER");
-	    System.out.println("Send order 2 server succeed.");
-	    String resp = in.readLine();
-	    System.out.println("Now is : " + resp);
-	} catch (Exception e) {
-	    e.printStackTrace();
-	} finally {
-	    if (out != null) {
-		out.close();
-		out = null;
-	    }
-
-	    if (in != null) {
-		try {
-		    in.close();
-		} catch (IOException e) {
-		    e.printStackTrace();
-		}
-		in = null;
-	    }
-
-	    if (socket != null) {
-		try {
-		    socket.close();
-		} catch (IOException e) {
-		    e.printStackTrace();
-		}
-		socket = null;
-	    }
-	}
+        int port = 8080;
+        Socket socket = null;
+        try {
+            socket = new Socket("127.0.0.1", port);
+            OutputStream os = socket.getOutputStream();
+            PrintWriter pw = new PrintWriter(os);
+            pw.write("我是数据1234567");
+            pw.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
--- src/com/phei/netty/bio/TimeServerHandler.java
+++ src/com/phei/netty/bio/TimeServerHandler.java
@@ -34,50 +34,31 @@ public class TimeServerHandler implements Runnable {
 	this.socket = socket;
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.lang.Runnable#run()
-     */
     @Override
     public void run() {
 	BufferedReader in = null;
-	PrintWriter out = null;
 	try {
-	    in = new BufferedReader(new InputStreamReader(
-		    this.socket.getInputStream()));
-	    out = new PrintWriter(this.socket.getOutputStream(), true);
-	    String currentTime = null;
-	    String body = null;
+	    in = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
 	    while (true) {
-		body = in.readLine();
-		if (body == null)
-		    break;
-		System.out.println("The time server receive order : " + body);
-		currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ? new java.util.Date(
-			System.currentTimeMillis()).toString() : "BAD ORDER";
-		out.println(currentTime);
-	    }
-
+			String message = in.readLine();
+			if (message != null) {
+				System.out.println("服务端收到消息:"+message);
+			}
+		}
 	} catch (Exception e) {
 	    if (in != null) {
-		try {
-		    in.close();
-		} catch (IOException e1) {
-		    e1.printStackTrace();
-		}
-	    }
-	    if (out != null) {
-		out.close();
-		out = null;
+			try {
+				in.close();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
 	    }
 	    if (this.socket != null) {
-		try {
-		    this.socket.close();
-		} catch (IOException e1) {
-		    e1.printStackTrace();
-		}
-		this.socket = null;
+			try {
+				this.socket.close();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
 	    }
 	}
     }
